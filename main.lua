1b9e4fe4beaab550bf430122e8f821650a20d8e6
LaneS2 <jenisilva23711@gmail.com>
2025-04-01 11:26:06 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,11 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/UI-Libraries/main/Vynixius/Source.lua"))()
+
+-- Create Window
+local Window = Library:AddWindow({
+	title = {"Vynixius", "UI Library"},
+	theme = {
+		Accent = Color3.fromRGB(0, 255, 0)
+	},
+	key = Enum.KeyCode.RightControl,
+	default = true
+})
